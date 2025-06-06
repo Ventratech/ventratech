b68ab6e5d2fc4d3b9bd0cda6741dae25181413e4
Alain Demblon <demblon@gmail.com>
2025-06-06 02:53:06 +0200

    fix: re-add ventratech-cms as a regular directory

---
+++ ventratech-cms/config/middlewares.ts
@@ -0,0 +1,20 @@
+export default [
+  'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // your Next.js frontend
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
+  'strapi::security',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+]
